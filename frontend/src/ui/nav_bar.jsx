cdd9ad7a9af8cc9e98addf292435380499d1f5cb
Aaronica123 <k.aaronmutua@gmail.com>
2026-07-11 10:11:59 +0300

    side bar

--- frontend/src/ui/nav_bar.jsx
+++ frontend/src/ui/nav_bar.jsx
@@ -1,17 +1,37 @@
-import { Nav_button } from "./buttons";
+import Execute_Button, { Nav_button } from "./buttons";
 import {Menu} from "lucide-react";
-import { Separator } from "@radix-ui/themes/dist/cjs/index.js";
+import { Avatar, Separator } from "@radix-ui/themes/dist/cjs/index.js";
 import { Head_Text } from "./text";
 import { Bell } from "lucide-react";
 
 export default function Nav_bar({children}){
 return(<>
-<div style={{width:"100vw",height:"100vh", display:"flex",flexDirection:"column"}}>
-        <div style={{width:"100%",height:"fit-content",display:"flex",
+<div style={{width:"100vw",height:"100vh", display:"flex",flexDirection:"row"}}>
+    <div style={{width:"fit-content",display:"flex",height:"100%",flexDirection:"column",justifyContent:"left",
+        background:"linear-gradient(180deg,white,black)",gap:"10px"}}>
+            <div style={{width:"100%",height:"fit-content",display:'flex',justifyContent:"left",padding:"15px"}}>
+            <Avatar fallback="KM" size={"6"} src="" color="blue" radius="medium"></Avatar>    
+            </div> 
+            <div style={{width:"100%",height:"fit-content", display:"flex",justifyContent:'left',flexWrap:"wrap"}}>
+            <Head_Text>ICT Help Desk</Head_Text>    
+            </div>
+            <div style={{width:"100%",height:"100%", top:"10",display:"flex",
+                flexDirection:"column",flexWrap:"nowrap",overflow:'auto',gap:"15px",
+                border:"solid 3px pink",borderLeft:"0px",borderRight:"0px",borderWidth:"100%",padding:"10px"}}>
+            
+            </div> 
+            <div style={{width:"100%",height:"fit-content",display:"flex",padding:"15px"}}>
+                <Execute_Button variant={"solid"} type={"button"} color={"blue"} onClick={""}>Execute</Execute_Button>
+            </div>
+     </div> 
+            <div style={{width:"100%",height:"100%",display:"flex",flexDirection:"column"}}> 
+                <div style={{width:"100%",height:"fit-content",display:"flex",
             flexDirection:"row",background:"linear-gradient(180deg, grey,white,grey)",padding:"10px",
-            alignItems:"start",borderBottomLeftRadius:"10px",borderBottomRightRadius:"10px"}}>
+            alignItems:"start"}}>
+            
+           
             <div style={{width:"100%",height:"fit-content",display:"flex" ,margin:"15px",flex:2.5}}>
-                <Nav_button variant={"ghost"}><Menu size={"30"} color="black"></Menu></Nav_button>
+                {/* <Nav_button variant={"ghost"}><Menu size={"30"} color="black"></Menu></Nav_button> */}
             </div>
             <div style={{width:"100%",height:"fit-content",display:"flex",
                flexDirection:"row",gap:"30px",margin:"10px",alignItems:"flex-end",flex:1.5}}>
@@ -26,10 +46,13 @@ return(<>
                 <Nav_button variant={"ghost"}><Bell size={"30"} color="black"></Bell></Nav_button>
                 </div>
             </div>
-        </div>
+             </div>
+        
         <div style={{width:"100%", display:"flex",height:"100%"}}>
             {children}
         </div>
+        </div>
+       
 </div>
 </>)
 }
